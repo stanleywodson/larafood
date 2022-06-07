83b797d6e0881d53d5391206f78dda5b542455ac
stanley wodson <stanley@DESKTOP-VQ11QOJ.localdomain>
2022-06-07 13:19:29 -0300

    migrate

--- database/migrations/2022_06_02_143101_create_permission_profile_table.php
+++ database/migrations/2022_06_02_143101_create_permission_profile_table.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Models\Profile;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Permission;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
